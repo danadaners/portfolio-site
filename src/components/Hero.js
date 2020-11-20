0e0596bd5849cb3781a8b75b37de7cee495c75a4
Dana Zhang <danadaners@gmail.com>
2020-11-20 00:00:15 -0500

    added manifest to site

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ const Hero = () => {
 
       <div className="hero-wrapper">
         <div id="hero-body">
-        <h1 id="greet">Hi, I'm Dana,</h1>
+        <h1 id="greet">Hi! I'm Dana,</h1>
         <h3 id="subgreet">a full stack web developer.</h3></div>
         </div>
     )
